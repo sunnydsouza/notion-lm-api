ee0fa4e665b930349372a6e3e7d4a0750009df51
sunnydsouza <dsouzasunny_1436@yahoo.co.in>
2022-05-03 22:38:21 +0530

    chore: added sample certs README.md and secrets_file.sample.py

---
+++ configuration/secrets_file.sample.py
@@ -0,0 +1,22 @@
+token = ""
+version = "2021-08-16"
+
+month_database_id = ""
+week_database_id = ""
+diary_database_id = ""
+
+logged_hours_database_id = ""
+master_task_database_id = ""
+personal_growth_database_id = ""
+features_database_id = ""
+releases_database_id = ""
+
+
+# used for notion page filers - NOT OFFICIAL API
+todays_task_filter_gtd_viewid=""
+session_hrs_filter_gtd_viewid=""
+
+# used for notion page filers - NOT OFFICIAL API
+notion_client_version = ''
+x_notion_active_user_header = ''
+cookie = ''
